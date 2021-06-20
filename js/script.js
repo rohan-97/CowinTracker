0f2a04fbbc9b4a3901e16a285f9b42529194362a
rohan-97 <yrohan31@gmail.com>
2021-06-20 19:38:42 +0530

    Removed Async Await stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {
+        accordion: true
+    });
+});
 info = {}
-async function populate_form() {
-    chrome.storage.sync.get(['cowin_tracker'], result => {
+function populate_form() {
+    tempo = chrome.storage.sync.get(['cowin_tracker'], result => {
         if(result.cowin_tracker instanceof Object){
             info['pincode'] = result.cowin_tracker.pincode;
             info['min_age'] = result.cowin_tracker.min_age;
@@ -10,101 +17,92 @@ async function populate_form() {
             set_min_age(info['min_age']);
             set_vaccines_filter(info['vaccine_filter']);
             set_cost(info['cost_filter']);
+            console.log("Rohan Debug 1 " +window.performance.now());
+            get_vaccination_centres(info);
         }
     });
-    return true;
 }
-populate_form().then(
-    (value) => {
-        fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=421301&date=19-06-2021')
-        .then(response => response.json())
-        .then(data => {
-            vaccine_center_ul_list = document.getElementById('vaccine_centre_list');
-            for(centre of data.sessions) {
-                vaccine_center_ul_list.appendChild(prepare_list_tag(centre));
-            }
-        }).catch(
-            error => {
-                error_text = "Jhol zala "+error;
-                console.log(error_text);
-                vaccine_center_ul_list = document.getElementById('vaccine_centre_list');
-                vaccine_center_ul_list.appendChild(prepare_error_message(error_text));            
-            }
-            );
-            document.addEventListener('DOMContentLoaded', function() {
-                var elems = document.querySelectorAll('.collapsible');
-                var instances = M.Collapsible.init(elems, {
-                    accordion: true
-                });
-            });
-        },
-    (error) => {
+populate_form()
+
+function get_vaccination_centres(info) {
+    today = new Date();
+    date_string = today.getDate()+'-'+(today.getMonth() + 1)+'-'+today.getFullYear()
+    pincode_url = new URL('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin');
+    pincode_url.searchParams.set('pincode', info['pincode']);
+    pincode_url.searchParams.set('date', date_string);
+    url_to_be_called = String(pincode_url);
+    console.log(url_to_be_called);
+    fetch(url_to_be_called)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        vaccine_center_ul_list = document.getElementById('vaccine_centre_list');
+        for(centre of data.sessions) {
+            console.log("Processing " + centre)
+            vaccine_center_ul_list.appendChild(prepare_list_tag(centre));
+        }
+    }).catch(
+        error => {
             error_text = "Jhol zala "+error;
-            console.log(error_text);
             vaccine_center_ul_list = document.getElementById('vaccine_centre_list');
-            vaccine_center_ul_list.appendChild(prepare_error_message(error_text));
-            
-            document.addEventListener('DOMContentLoaded', function() {
-            var elems = document.querySelectorAll('.collapsible');
-            var instances = M.Collapsible.init(elems, {
-                accordion: true
-            });
-        });
+            vaccine_center_ul_list.appendChild(prepare_error_message(error_text));            
+        }
+        );
 
     }
-    )
-
+    
+    
     function prepare_error_message(error_msg){
         list_tag = document.createElement('li');
-            error_message = document.createElement('div');
-            error_message.classList.add("collapsible-header")
-                error_icon = document.createElement('i');
-                error_icon.classList.add('material-icons');
-                error_icon.innerText = 'error'
-            error_message.appendChild(error_icon)
-                name_text = document.createTextNode(error_msg);
-            error_message.appendChild(name_text)
+        error_message = document.createElement('div');
+        error_message.classList.add("collapsible-header")
+        error_icon = document.createElement('i');
+        error_icon.classList.add('material-icons');
+        error_icon.innerText = 'error'
+        error_message.appendChild(error_icon)
+        name_text = document.createTextNode(error_msg);
+        error_message.appendChild(name_text)
         list_tag.appendChild(error_message);
         return list_tag
     }
     function prepare_list_tag(centre){
         list_tag = document.createElement('li');
-            name_tag = document.createElement('div');
-            name_tag.classList.add("collapsible-header")
-                hosp_icon = document.createElement('i');
-                hosp_icon.classList.add('material-icons');
-                hosp_icon.innerText = 'local_hospital'
-            name_tag.appendChild(hosp_icon)
-                name_text = document.createTextNode(centre.name);
-            name_tag.appendChild(name_text)
+        name_tag = document.createElement('div');
+        name_tag.classList.add("collapsible-header")
+        hosp_icon = document.createElement('i');
+        hosp_icon.classList.add('material-icons');
+        hosp_icon.innerText = 'local_hospital'
+        name_tag.appendChild(hosp_icon)
+        name_text = document.createTextNode(centre.name);
+        name_tag.appendChild(name_text)
         list_tag.appendChild(name_tag)
-            collapse_body_tag = document.createElement('div');
-            collapse_body_tag.classList.add('collapsible-body');
-                ul_tag = document.createElement('ul');
-                    age_list = document.createElement('li')
-                    age_list.innerText = "Age Limit : "+centre.min_age_limit+"+";
-                ul_tag.appendChild(age_list);
-                    vaccine_name_tag = document.createElement('li');
-                    vaccine_name_tag.innerText = "Vaccine Name : " + centre.vaccine;
-                ul_tag.appendChild(vaccine_name_tag);
-                    dose_1_tag = document.createElement('li');
-                    dose_1_tag.innerText = "Dose 1 : " + centre.available_capacity_dose1;
-                ul_tag.appendChild(dose_1_tag);
-                    dose_2_tag = document.createElement('li');
-                    dose_2_tag.innerText = "Dose 2 : " + centre.available_capacity_dose2;
-                ul_tag.appendChild(dose_2_tag);
-                    fee_tag = document.createElement('li');
-                    fee_tag.innerText = "Fee : " + centre.fee;
-                ul_tag.appendChild(fee_tag);
-            collapse_body_tag.appendChild(ul_tag);
-                address_tag = document.createElement('p');
-                address_tag.innerText = centre.address
-            collapse_body_tag.appendChild(address_tag);
+        collapse_body_tag = document.createElement('div');
+        collapse_body_tag.classList.add('collapsible-body');
+        ul_tag = document.createElement('ul');
+        age_list = document.createElement('li')
+        age_list.innerText = "Age Limit : "+centre.min_age_limit+"+";
+        ul_tag.appendChild(age_list);
+        vaccine_name_tag = document.createElement('li');
+        vaccine_name_tag.innerText = "Vaccine Name : " + centre.vaccine;
+        ul_tag.appendChild(vaccine_name_tag);
+        dose_1_tag = document.createElement('li');
+        dose_1_tag.innerText = "Dose 1 : " + centre.available_capacity_dose1;
+        ul_tag.appendChild(dose_1_tag);
+        dose_2_tag = document.createElement('li');
+        dose_2_tag.innerText = "Dose 2 : " + centre.available_capacity_dose2;
+        ul_tag.appendChild(dose_2_tag);
+        fee_tag = document.createElement('li');
+        fee_tag.innerText = "Fee : " + centre.fee;
+        ul_tag.appendChild(fee_tag);
+        collapse_body_tag.appendChild(ul_tag);
+        address_tag = document.createElement('p');
+        address_tag.innerText = centre.address
+        collapse_body_tag.appendChild(address_tag);
         list_tag.appendChild(collapse_body_tag);
         return list_tag;
     }
     document.getElementById('settings_save').onclick = save_settings;
-        
+    
     function save_settings() {
         info['pincode'] = get_pincode_from_inputtext();
         info['min_age'] = get_min_age();
